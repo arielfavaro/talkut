ad5fb1eeaa1e6a855a6907337fdc60ec29284cae
Ariel Favaro <arieltainan@gmail.com>
2021-07-17 22:42:28 -0300

    added github domain to images

--- next.config.js
+++ next.config.js
@@ -6,4 +6,7 @@ module.exports = {
         config.resolve.alias['@'] = path.resolve(__dirname, './')
         return config
     },
+    images: {
+        domains: ['avatars.githubusercontent.com'],
+    },
 }
